78c9f584507ca5ad3d39259c413d6c69b0cfc61b
Scott Gould <greysilly7@gmail.com>
2022-07-04 20:44:45 -0400

    Make mixin less invasive

--- src/main/java/me/greysilly7/salir/mixin/ServerHandshakeNetworkHandlerMixin.java
+++ src/main/java/me/greysilly7/salir/mixin/ServerHandshakeNetworkHandlerMixin.java
@@ -13,18 +13,19 @@ import net.minecraft.server.network.ServerQueryNetworkHandler;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import org.apache.commons.lang3.Validate;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Objects;
 import java.util.Optional;
@@ -32,74 +33,33 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @Mixin(ServerHandshakeNetworkHandler.class)
 public class ServerHandshakeNetworkHandlerMixin {
-    private static final Text IGNORING_STATUS_REQUEST_MESSAGE = Text.literal("Ignoring status request");
-    private final MinecraftServer server;
-    private final ClientConnection connection;
-
-    public ServerHandshakeNetworkHandlerMixin(MinecraftServer server, ClientConnection connection) {
-        this.server = server;
-        this.connection = connection;
-    }
-
+    @Shadow @Final private MinecraftServer server;
 
     private static <T> T getRandomElement(T[] arr){
         return arr[ThreadLocalRandom.current().nextInt(arr.length)];
     }
 
-    /**
-     * @author Greysilly7
-     * @reason I need to set custom image :P
-     */
-    @Overwrite()
-    public void onHandshake(HandshakeC2SPacket packet) {
-        switch (packet.getIntendedState()) {
-            case LOGIN -> {
-                this.connection.setState(NetworkState.LOGIN);
-                if (packet.getProtocolVersion() != SharedConstants.getGameVersion().getProtocolVersion()) {
-                    MutableText text;
-                    if (packet.getProtocolVersion() < 754) {
-                        text = Text.translatable("multiplayer.disconnect.outdated_client", new Object[]{SharedConstants.getGameVersion().getName()});
-                    } else {
-                        text = Text.translatable("multiplayer.disconnect.incompatible", new Object[]{SharedConstants.getGameVersion().getName()});
-                    }
+    @Inject(method = "onHandshake", at = @At("HEAD"))
+    public void onHandshake(HandshakeC2SPacket packet, CallbackInfo ci) {
+        File servericonsDir = new File("./server-icons/");
+        if (Objects.requireNonNull(servericonsDir.listFiles())[0].exists()) {
+            Optional<File> optional = Optional.of(getRandomElement(Objects.requireNonNull(servericonsDir.listFiles()))).filter(File::isFile);
 
-                    this.connection.send(new LoginDisconnectS2CPacket(text));
-                    this.connection.disconnect(text);
-                } else {
-                    this.connection.setPacketListener(new ServerLoginNetworkHandler(this.server, this.connection));
+            optional.ifPresent((file) -> {
+                try {
+                    BufferedImage bufferedImage = ImageIO.read(file);
+                    Validate.validState(bufferedImage.getWidth() == 64, "Must be 64 pixels wide");
+                    Validate.validState(bufferedImage.getHeight() == 64, "Must be 64 pixels high");
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    ImageIO.write(bufferedImage, "PNG", byteArrayOutputStream);
+                    byte[] bs = Base64.getEncoder().encode(byteArrayOutputStream.toByteArray());
+                    String var10001 = new String(bs, StandardCharsets.UTF_8);
+                    this.server.getServerMetadata().setFavicon("data:image/png;base64," + var10001);
+                } catch (Exception var5) {
+                    ServerListImageRandomizer.LOGGER.error("Couldn't load server icon", var5);
                 }
-            }
-            case STATUS -> {
-                if (this.server.acceptsStatusQuery()) {
-                    File servericonsDir = new File("./server-icons/");
-                    if (servericonsDir.listFiles()[0].exists()) {
-                        Optional<File> optional = Optional.of(getRandomElement(Objects.requireNonNull(servericonsDir.listFiles()))).filter(File::isFile);
-
-                        optional.ifPresent((file) -> {
-                            try {
-                                BufferedImage bufferedImage = ImageIO.read(file);
-                                Validate.validState(bufferedImage.getWidth() == 64, "Must be 64 pixels wide");
-                                Validate.validState(bufferedImage.getHeight() == 64, "Must be 64 pixels high");
-                                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                                ImageIO.write(bufferedImage, "PNG", byteArrayOutputStream);
-                                byte[] bs = Base64.getEncoder().encode(byteArrayOutputStream.toByteArray());
-                                String var10001 = new String(bs, StandardCharsets.UTF_8);
-                                server.getServerMetadata().setFavicon("data:image/png;base64," + var10001);
-                            } catch (Exception var5) {
-                                ServerListImageRandomizer.LOGGER.error("Couldn't load server icon", var5);
-                            }
-
-                        });
-                    }
-                    this.connection.setState(NetworkState.STATUS);
-                    this.connection.setPacketListener(new ServerQueryNetworkHandler(this.server, this.connection));
-                } else {
-                    this.connection.disconnect(IGNORING_STATUS_REQUEST_MESSAGE);
-                }
-            }
-            default -> throw new UnsupportedOperationException("Invalid intention " + packet.getIntendedState());
+            });
         }
-
     }
 }
 
