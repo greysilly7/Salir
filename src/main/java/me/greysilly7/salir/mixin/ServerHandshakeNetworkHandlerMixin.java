5dd4feff546e3b5c6e60fe1f2159e8ee92eeecf3
Scott Gould <greysilly7@gmail.com>
2022-07-12 23:31:11 -0400

    Write resized image instead of resizing each time wasting resources

--- src/main/java/me/greysilly7/salir/mixin/ServerHandshakeNetworkHandlerMixin.java
+++ src/main/java/me/greysilly7/salir/mixin/ServerHandshakeNetworkHandlerMixin.java
@@ -24,42 +24,49 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @Mixin(ServerHandshakeNetworkHandler.class)
 public class ServerHandshakeNetworkHandlerMixin {
-    @Shadow @Final private MinecraftServer server;
+	@Shadow
+	@Final
+	private MinecraftServer server;
 
-    private static <T> T getRandomElement(T[] arr){
-        return arr[ThreadLocalRandom.current().nextInt(arr.length)];
-    }
+	private static <T> T getRandomElement(T[] arr) {
+		return arr[ThreadLocalRandom.current().nextInt(arr.length)];
+	}
 
-    private static BufferedImage imageToBufferedImage(Image im) {
-        BufferedImage bi = new BufferedImage
-                (im.getWidth(null),im.getHeight(null),BufferedImage.TYPE_INT_RGB);
-        Graphics bg = bi.getGraphics();
-        bg.drawImage(im, 0, 0, null);
-        bg.dispose();
-        return bi;
-    }
+	private static BufferedImage imageToBufferedImage(Image im) {
+		BufferedImage bi = new BufferedImage(im.getWidth(null), im.getHeight(null), BufferedImage.TYPE_INT_RGB);
+		Graphics bg = bi.getGraphics();
+		bg.drawImage(im, 0, 0, null);
+		bg.dispose();
+		return bi;
+	}
 
+	@Inject(method = "onHandshake", at = @At("HEAD"))
+	public void onHandshake(HandshakeC2SPacket packet, CallbackInfo ci) {
+		File servericonsDir = new File("./server-icons/");
+		if (Objects.requireNonNull(servericonsDir.listFiles())[0].exists()) {
+			Optional<File> optional = Optional.of(getRandomElement(Objects.requireNonNull(servericonsDir.listFiles())))
+					.filter(File::isFile);
 
-    @Inject(method = "onHandshake", at = @At("HEAD"))
-    public void onHandshake(HandshakeC2SPacket packet, CallbackInfo ci) {
-        File servericonsDir = new File("./server-icons/");
-        if (Objects.requireNonNull(servericonsDir.listFiles())[0].exists()) {
-            Optional<File> optional = Optional.of(getRandomElement(Objects.requireNonNull(servericonsDir.listFiles()))).filter(File::isFile);
+			optional.ifPresent((file) -> {
+				try {
+					BufferedImage bufferedImage = ImageIO.read(file);
 
-            optional.ifPresent((file) -> {
-                try {
-                    BufferedImage bufferedImage = ImageIO.read(file);
-                    Image scaled = bufferedImage.getScaledInstance(64, 64, Image.SCALE_SMOOTH);
+					if (bufferedImage.getWidth() != 64 || bufferedImage.getHeight() != 64) {
+						bufferedImage = imageToBufferedImage(
+								bufferedImage.getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+						// Write bufferedImage to ./server-icons/<original-name>.png
+						ImageIO.write(bufferedImage, "PNG", file);
+					}
 
-                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                    ImageIO.write(imageToBufferedImage(scaled), "PNG", byteArrayOutputStream);
-                    byte[] bs = Base64.getEncoder().encode(byteArrayOutputStream.toByteArray());
-                    String var10001 = new String(bs, StandardCharsets.UTF_8);
-                    this.server.getServerMetadata().setFavicon("data:image/png;base64," + var10001);
-                } catch (Exception var5) {
-                    ServerListImageRandomizer.LOGGER.error("Couldn't load server icon", var5);
-                }
-            });
-        }
-    }
+					ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+					ImageIO.write(bufferedImage, "PNG", byteArrayOutputStream);
+					byte[] bs = Base64.getEncoder().encode(byteArrayOutputStream.toByteArray());
+					String var10001 = new String(bs, StandardCharsets.UTF_8);
+					this.server.getServerMetadata().setFavicon("data:image/png;base64," + var10001);
+				} catch (Exception var5) {
+					ServerListImageRandomizer.LOGGER.error("Couldn't load server icon", var5);
+				}
+			});
+		}
+	}
 }
